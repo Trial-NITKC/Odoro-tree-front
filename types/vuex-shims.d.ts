f5c772c0ee13c90b1aa030da0aeec165420b3d9b
tos-up <yoshikawa@c-style.net>
2022-12-13 20:08:33 +0900

    chore: type define

--- types/vuex-shims.d.ts
+++ types/vuex-shims.d.ts
@@ -1,25 +1,26 @@
-import { Repositories } from '~/plugins/apiFactory'
-import { accessorType } from '~/store'
+import { Repositories } from '~/plugins/apiFactory';
+import { accessorType } from '~/store';
 
 declare module 'vuex/types/index' {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface Store<S> {
-    readonly $repositories: Repositories
+    readonly $repositories: Repositories;
   }
 }
 
 declare module 'vue/types/vue' {
   interface Vue {
-    $accessor: typeof accessorType
-    readonly $repositories: Repositories
+    $accessor: typeof accessorType;
+    readonly $repositories: Repositories;
   }
 }
 
 declare module '@nuxt/types' {
   interface NuxtAppOptions {
-    $accessor: typeof accessorType
+    $accessor: typeof accessorType;
   }
 
   interface Context {
-    $accessor: typeof accessorType
+    $accessor: typeof accessorType;
   }
 }
